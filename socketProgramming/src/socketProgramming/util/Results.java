bf48e656780905f0ab3b0dd4950cd071bc457159
aanvekar21 <aanveka2@binghamton.edu>
2020-03-29 00:12:13 -0400

    Minor changes in Results Class

--- socketProgramming/src/socketProgramming/util/Results.java
+++ socketProgramming/src/socketProgramming/util/Results.java
@@ -5,39 +5,34 @@ import java.util.List;
 import socketProgramming.util.MyLogger.DebugLevel;
 
 /**
- * The Class Results store the result of Prime numbers in an ArrayList Data Structure and 
- *  prints on console 
+ * The Class Results store the result of Prime numbers in an ArrayList 
+ * Data Structure and prints on console
+ * 
  * @author Akshay Anvekar and Kenneth Fernandes
  */
-public class Results 
-{
+public class Results {
 	private List<Integer> primeNumList;
-	
-	public Results()
-	{
+
+	public Results() {
 		MyLogger.writeMessage("Results()", DebugLevel.CONSTRUCTOR);
 		primeNumList = new ArrayList<Integer>();
 	}
-	
+
 	/**
-	 * A thread safe method for adding the prime number read from
-	 *  the file to the list of prime numbers
+	 * A thread safe method for adding the prime number read from the file to the
+	 * list of prime numbers
+	 * 
 	 * @param primeNo The prime number to be added to the list
 	 */
-	public synchronized void addPrimeNum(int primeNum)
-	{
+	public synchronized void addPrimeNum(int primeNum) {
 		MyLogger.writeMessage("addPrimeNo()", DebugLevel.RESULTS_ADDED);
-        	primeNumList.add(primeNum);
-        	for (int i =0;i < primeNumList.size(); i++){
-					primeNumList.get(i);
-			
-            }
+		primeNumList.add(primeNum);
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		System.out.println(primeNumList.toString());
 		return "Results: " + primeNumList.toString();
 	}
 }
+
